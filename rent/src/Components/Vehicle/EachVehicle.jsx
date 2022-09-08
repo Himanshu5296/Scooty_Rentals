ba997faab562d14a90fbe85210a6473c417b53e4
Himanshu5296 <himanshu.singh5296@gmail.com>
2022-09-08 21:59:29 +0530

    timer added

--- rent/src/Components/Vehicle/EachVehicle.jsx
+++ rent/src/Components/Vehicle/EachVehicle.jsx
@@ -1,9 +1,33 @@
 import React, { useState } from 'react'
 import style from "./vehicle.module.css"
 
+let minutes = 1
+let seconds = 60
+
 const EachVehicle = (props) => {
     const [book,setbook] = useState("book")
-    const [countdown] = useState(2)
+    const [[mins, secs], setTime] = useState([minutes,seconds]);
+
+    const tick = () => {
+   
+        if (mins === 0 && secs === 0) 
+            reset()
+        else if (secs === 0) {
+            setTime([mins - 1, 59]);
+        } else {
+            setTime([mins, secs - 1]);
+        }
+    };
+    
+    const reset = () => setTime([parseInt(minutes), parseInt(seconds)]);
+    
+        
+    React.useEffect(() => {
+        const timerId = setInterval(() => tick(), 1000);
+        return () => clearInterval(timerId);
+    });
+
+
   return (
     <div className={style.eachVehicleDetaile}> 
        <img src={props.image} alt="" />
@@ -14,7 +38,9 @@ const EachVehicle = (props) => {
         ) : book==="pick" ? 
         (
             <>
-            <p>{countdown}</p>
+            <p>{`${mins
+            .toString()
+            .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p>
             <button onClick={()=>setbook("booked")}>Pick</button>
             </>
         ): book==="booked" ? (
